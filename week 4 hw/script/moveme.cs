5d97f132f18d4bcd3e01d67a9b698dbbef488cd1
Dhiggins94 <dhiggins94@gmail.com>
2016-05-27 15:06:39 -0400

    Add files via upload

---
+++ week 4 hw/script/moveme.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveme : MonoBehaviour {
+	public Vector3 speed = new Vector3 (0f,0f,0f);
+	public GameObject[] moveables;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+
+		Debug.Log (GetComponent<Transform> ().position);
+
+		//instead of doing GameObject.Getcomponent<Transform>().......
+		//GetComponent<Transform> ().position += speed;
+
+
+		//for (int i = 0; i < someNumber; i++){
+		foreach (GameObject thingtomove in	 moveables) {
+			thingtomove.transform.Translate(speed);
+		}
+	}
+}
---
+++ week 5/folder1/gladeat.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gladeat : MonoBehaviour {
+	public GameObject plaque; 
+	public Vector3 destination;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		destination = plaque.transform.position;
+
+		Vector3 direction =  Vector3.Normalize (destination - transform.position);
+
+		Debug.Log ("direction = " + direction); 
+
+		GetComponent<Rigidbody> ().AddForce (direction * 5);
+	}
+	void onCollisionEnter(Collision collision){
+		Debug.Log ("collision = " + collision.collider);
+		if(plaque){
+			Destroy (collision.gameObject);
+
+	}
+}
+	}
